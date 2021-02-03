1d6a19d00ca64429f23bd7557c7f3c195b24ee14
Hasinul-Alam <74594573+Hasinul-Alam@users.noreply.github.com>
2021-02-04 00:49:06 +0600

    Update estLive.js

--- build/js/estLive.js
+++ build/js/estLive.js
@@ -26,10 +26,10 @@ let weeks = {
 }
 
 let currentDate = new Date()
-let utc_offset = currentDate.getTimezoneOffset();
-currentDate.setMinutes(currentDate.getMinutes() + utc_offset);
-let usa_offset = 5*60;
-currentDate.setMinutes(currentDate.getMinutes() - usa_offset);
+// let utc_offset = currentDate.getTimezoneOffset();
+// currentDate.setMinutes(currentDate.getMinutes() + utc_offset);
+// let usa_offset = 5*60;
+// currentDate.setMinutes(currentDate.getMinutes() - usa_offset);
 console.log(currentDate);
 let found = false;
 
@@ -42,7 +42,7 @@ for (const week in weeks) {
         document.getElementById(week).remove();
     } 
 }
-if(found !== false){
-    document.getElementById('default').remove();
-}
+// if(found !== false){
+//     document.getElementById('default').remove();
+// }
 
